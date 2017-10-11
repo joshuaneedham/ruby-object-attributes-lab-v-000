80389ae2e2113b39bfaf2530a0fcc26367c81b1a
Joshua Needham <joshuabneedham@gmail.com>
2017-10-11 17:18:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,9 @@ class Dog
   def initialize(name)
     @name = name
   end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
+  
+  def name=(name)
+    @name = name
   end
 
   def name
